a2b4efc8191979908c9f6c38005e475c3af0915b
V1tal1y <vitalik-Shulgin@yandex.ru>
2020-08-22 23:52:05 +0300

    if, ? , swich

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 "use strict"
 
-let numberOfFilms
-numberOfFilms = +prompt('Сколько фильмов вы посмотрели')
+let numberOfFilms,
+    moreThan,
+    lvl;
+
+numberOfFilms = prompt('Сколько фильмов вы посмотрели','');
+
+(numberOfFilms > 1) ? moreThan = true  :  moreThan = false;
+
+switch (numberOfFilms){
+    case 1:
+        lvl = 'noob';
+        break;
+    case 2:
+        lvl = 'noob mini';
+        break;
+    case 3:
+        lvl = 'normal';
+        break;
+    default:
+        lvl = 'pro';
+        break;
+}
 
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: {},
+    genres: [],
+    moreThan,
+    lvl,
     privat: false
 }
 
 for(let i = 0; i < 2; i++ ) {
-    let nameFilm = prompt('Название фильма'),
-        filmScore = prompt('оценка')
+    let nameFilm = prompt('Название фильма',''),
+        filmScore = prompt('оценка','')
     personalMovieDB.movies[nameFilm] = filmScore
 }
 
