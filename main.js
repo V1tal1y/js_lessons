19ff0f88f9db7a7e095a1542b1e94d7e1521dca1
V1tal1y <vitalik-Shulgin@yandex.ru>
2020-08-22 23:35:28 +0300

    less1

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 "use strict"
 
-console.log('123')
+let numberOfFilms
+numberOfFilms = +prompt('Сколько фильмов вы посмотрели')
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+}
+
+for(let i = 0; i < 2; i++ ) {
+    let nameFilm = prompt('Название фильма'),
+        filmScore = prompt('оценка')
+    personalMovieDB.movies[nameFilm] = filmScore
+}
+
+console.log(personalMovieDB)
